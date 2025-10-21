3574a81a5f0f57bab7ee73430fabd29d0f44dad8
IsabelaGarcia0 <isabela.g.menezes@aluno.senai.br>
2025-10-21 17:56:04 -0300

    Animação de carregamento

--- js/script.js
+++ js/script.js
@@ -9,3 +9,14 @@ function escreverComDelay(texto, delay = 30) {
     }
     console.log();
 }
+
+//  Animação de carregamento
+function animacaoCarregamento(frase, pontos = 3, tempo = 300) {
+    process.stdout.write(frase);
+    for (let i = 0; i < pontos; i++) {
+        process.stdout.write(".");
+        const inicio = Date.now();
+        while (Date.now() - inicio < tempo) {}
+    }
+    console.log();
+}
